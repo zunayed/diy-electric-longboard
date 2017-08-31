9399bfd24afa2fc937857c0b8f5799c7756ac015
Zunayed Ali <zali@tower-research.com>
2017-08-31 16:13:57 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,431 @@
+<template>
+  <div id="app">
+    <div class="container">
+
+			<div class="header">
+				<h1 class="page-title">DIY electric longboad</h1>
+			</div>
+
+			<div class="section row">
+				<div class="col-8">
+					<img src="http://via.placeholder.com/600x250">
+				</div>
+
+				<div class="col-4">
+					<h1 class="timberframing">Intro</h1>
+					<p>
+This page is a combination of a journal and notes I took while building an electric longboard
+					</p>
+				</div>
+			</div>
+
+			<div class="section row">
+				<div class="col-4">
+					<h1 class="timberframing">Parts</h1>
+					<p>
+The following are the different components we need to research, buy and use!
+Now you have to look at all the parts holistically. For example your wheels and battery size will dictate your range and top speed. The type of motor you use will have different characteristics for torque
+					</p>
+				</div>
+
+				<div class="col-8">
+          <div class="row">
+
+					<div class="col-md-2">
+            <img class="icon" src="https://png.icons8.com/skateboard-without-wheels/ios7/75" height="75">
+						<span class="title">Board</span>
+					</div>
+					<div class="col-md-2">
+            <img class="icon" src="https://png.icons8.com/skateboard-wheel/ios7/75" height="75">
+						<span class="title">Wheels</span>
+          </div>
+
+					<div class="col-md-2">
+            <img class="icon" src="https://png.icons8.com/skateboard-truck/ios7/75" height="75">
+						<span class="title">Trucks</span>
+          </div>
+
+					<div class="col-md-2">
+            <img class="icon" src="https://png.icons8.com/attach/ios7/75" height="75">
+						<span class="title">Mount</span>
+          </div>
+
+					<div class="col-md-2">
+            <img class="icon" src="https://png.icons8.com/pulley/ios7/75" height="75">
+						<span class="title">Pulley</span>
+          </div>
+
+					<div class="col-md-2">
+            <img class="icon" src="https://png.icons8.com/settings/ios7/75" height="75">
+						<span class="title">belts</span>
+          </div>
+
+					<div class="col-md-2">
+            <img class="icon" src="https://png.icons8.com/engine/ios7/75" height="75">
+						<span class="title">Motor</span>
+          </div>
+
+					<div class="col-md-2">
+            <img class="icon" src="https://png.icons8.com/car-battery/ios7/75" height="75">
+						<span class="title">Battery</span>
+          </div>
+
+					<div class="col-md-2">
+            <img class="icon" src="https://png.icons8.com/electrical/ios7/75" height="75">
+						<span class="title">Charger</span>
+          </div>
+
+					<div class="col-md-2">
+            <img class="icon" src="https://png.icons8.com/motherboard/Dusk_Wired/64" height="75">
+						<span class="title">Controller</span>
+          </div>
+
+					<div class="col-md-2">
+            <img class="icon" src="https://png.icons8.com/remote-control/ios7/75" height="75">
+						<span class="title">Remote</span>
+          </div>
+
+					<div class="col-md-2">
+            <img class="icon" src="https://png.icons8.com/star-of-bethlehem/ios7/75" height="75">
+						<span class="title">Spark Protection</span>
+          </div>
+
+					<div class="col-md-2">
+            <img class="icon" src="https://png.icons8.com/spring-in-motion/ios7/75" height="75">
+						<span class="title">Wiring</span>
+          </div>
+
+					<div class="col-md-2">
+            <img class="icon" src="https://png.icons8.com/rca/ios7/75" height="75">
+						<span class="title">Connectors</span>
+          </div>
+
+					<div class="col-md-2">
+            <img class="icon" src="https://png.icons8.com/maintenance/ios7/75" height="75">
+						<span class="title">Tools</span>
+          </div>
+
+          </div>
+				</div>
+      </div>
+
+      <hr/>
+
+      <div class="section row">
+
+        <div class="col-xs-2">
+            <div class="green widget">
+              <span class="line1">Tuesday</span>
+              <span class="line2">Aug 29</span>
+							<hr/>
+              <p>
+								Started researching
+              </p>
+            </div>
+				</div>
+
+        <div class="col-xs-2">
+            <div class="top widget">
+              <span class="line1">Wednesday</span>
+              <span class="line2">Aug 30</span>
+							<hr/>
+              <p >
+								Ordered mount, pulley, belts, controller and spark protection!
+              </p>
+            </div>
+				</div>
+
+        <div class="col-xs-2">
+            <div class="top widget">
+              <span class="line1">Thursday</span>
+              <span class="line2">Aug 31</span>
+							<hr/>
+              <p >
+								Started building this website :)
+              </p>
+            </div>
+				</div>
+
+      </div>
+
+      <hr/>
+
+      <div class="section row">
+        <div class="col-4">
+          <div class="section-title">
+            <div class="timberframing">Board
+            <img class=""
+              src="https://png.icons8.com/skateboard-without-wheels/ios7/75"
+              height="35">
+            </div>
+          </div>
+
+          <p>
+            There are thousands of board you can choose from. You'll notice a lot of people on DIY builds tend to go with stiffer boards. That's because they don't want all the components like batteries to flex while riding. Space under your board is a precious resource and with large batteries, motors and controllers you'll find you're going to run out of space fast. You can try to separate out your battery and controllers to each of the board ends but you now can't use the middle of the board. For this build I chose to go with a stiff 44" longboard. Other things to consider is clearance!
+          </p>
+
+          <p>
+            <b>Final Choice: </b>Ten Toes Board Emporium Zed Bamboo Longboard Skateboard Cruiser
+          </p>
+        </div>
+
+				<div class="col-4">
+          <div class="section-title">
+            <div class="timberframing">Trucks
+            <img class=""
+              src="https://png.icons8.com/skateboard-truck/ios7/75"
+              height="35">
+            </div>
+          </div>
+          <p>
+            You need to make sure you have enough clearance to mount your motor and mount. I went with the Caliber II trucks. It seems to be a popular choice among builds and from a trusted brand.
+          </p>
+
+          <p>
+            <b>Final Choice: </b>Caliber Trucks Cal II 50° RKP
+          </p>
+        </div>
+
+				<div class="col-4">
+          <div class="section-title">
+            <div class="timberframing">Wheels
+            <img class=""
+              src="https://png.icons8.com/skateboard-wheel/ios7/75"
+              height="35">
+            </div>
+          </div>
+          <p>
+            Larger wheels give you more stability and comfort and helps in cities where the governments waste your tax dollars and don't fix shitty roads. The the larger the wheel the more top speed you can reach but at the expense of torque. 83mm seems to be a common size to use but I opted for 90mm to handle NYC.
+          </p>
+        </div>
+
+				<div class="col-12">
+          <div class="section-title">
+            <div class="timberframing">Mount, Pulley, Belt
+            <img class=""
+              src="https://png.icons8.com/attach/ios7/75"
+              height="35">
+            <img class=""
+              src="https://png.icons8.com/pulley/ios7/75"
+              height="35">
+            <img class=""
+              src="https://png.icons8.com/settings/ios7/75"
+              height="35">
+            </div>
+          </div>
+          <p>
+            These parts make up the drive train of an electric skateboard. They help transfer power from the motor to the wheels. There are some formulas and calculations you can do to help figure the configuration that is right for you.
+
+            The mount is simply a way to attach the motor to the truck. There are two pully teeth, one for the motor and one for the wheel. the motor teeth divided by the motor wheel gets you the gear ratio. This ratio in combination with wheel size, battery voltage and motor KV rating will help you calculate top speed. Jump to the motor and battery and motor section to understand how battery voltage and motor KV work.
+
+          </p>
+        </div>
+      </div>
+
+		</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data: () => ({
+    parts: [
+      {
+        title: "Board",
+        icon: "https://png.icons8.com/skateboard-without-wheels/ios7/75"
+      },
+      {
+        title: "Wheels",
+        icon: "https://png.icons8.com/skateboard-wheel/ios7/75"
+      },
+      {
+        title: "Trucks",
+        icon: "https://png.icons8.com/skateboard-truck/ios7/75"
+      },
+      {
+        title: "Mount",
+        icon: "https://png.icons8.com/attach/ios7/75"
+      },
+      {
+        title: "Pulley",
+      },
+      {
+        title: "Belts",
+      },
+      {
+        title: "Battery",
+      },
+      {
+        title: "Charger",
+      },
+      {
+        title: "Controller",
+      },
+      {
+        title: "Remote",
+      },
+      {
+        title: "Spark Protection",
+      },
+      {
+        title: "Wiring",
+      },
+      {
+        title: "connetors",
+      },
+      {
+        title: "Tools",
+      },
+    ],
+  })
+}
+</script>
+
+<style>
+
+body {
+	background-color: #f5f4f0;
+}
+
+.container {
+ width: 940px;
+}
+
+.section {
+	margin-bottom:35px;
+}
+
+.header {
+	margin-bottom: 70px;
+}
+
+.page-title {
+	font-family: "Apercu Medium Italic", Calibri, sans-serif;
+	font-size-adjust: 0.508;
+	font-weight: 600;
+	font-style: italic;
+
+	color: #282825;
+	text-transform: uppercase;
+	text-align: center;
+	font-size: 48px;
+	margin-top: 30px;
+}
+
+.timberframing {
+    font-family: "Apercu Italic", Calibri, sans-serif;
+    font-size-adjust: 0.508;
+    font-weight: normal;
+    font-style: italic;
+
+    font-size: 36px;
+    color: #6ac9cd;
+    margin-bottom: 0px;
+}
+
+.icon {
+  text-align: center;
+	padding: 10px;
+	display:block;
+}
+
+.title {
+  color: #282825;
+  font-size: 16px;
+  text-align: center;
+  line-height: 14px;
+  display: block;
+}
+
+.section-title {
+  text-align: center;
+}
+
+/*
+journal area
+*/
+
+
+.widget {
+	padding: 12px;
+	width: 150px;
+	height: 220px;
+}
+
+.widget.top {
+	margin-bottom: 20px;
+}
+
+.widget.green {
+	background-color: #b1e2c9;
+}
+
+.widget .line1 {
+	font-size-adjust: 0.508;
+	font-weight: normal;
+	font-style: normal;
+	display: block;
+}
+
+.widget .line2 {
+	font-size-adjust: 0.508;
+	font-weight: normal;
+	font-style: normal;
+
+	font-size: 24px;
+	color: #282825;
+	margin-bottom: 15px;
+}
+
+.widget .desc {
+    /*
+        FontDeck defaults
+    */
+    font-family: "Apercu Light", Calibri, sans-serif;
+    font-size-adjust: 0.508;
+    font-weight: 200;
+    font-style: normal;
+    line-height: 14px;
+
+    /*
+        Custom styling
+    */
+    color: #282825;
+    font-size: 12px;
+}
+
+.calendar-title {
+    /*
+        FontDeck defaults
+    */
+    font-family: "Apercu Italic", Calibri, sans-serif;
+    font-size-adjust: 0.508;
+    font-weight: normal;
+    font-style: italic;
+
+    /*
+        Custom styling
+    */
+    font-size: 24px;
+    color: #6ac9cd;
+}
+
+.calendar-desc {
+    /*
+        FontDeck defaults
+    */
+    font-family: "Apercu Light", Calibri, sans-serif;
+    font-size-adjust: 0.508;
+    font-weight: 200;
+    font-style: normal;
+    line-height: 1.2em;
+
+    /*
+        Custom styling
+    */
+    color: #282825;
+    font-size: 22px;
+}
+
+</style>
