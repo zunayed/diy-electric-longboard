2217c39e8861761dbd762366fdba2b68c3346aad
Zunayed Ali <zunayed@gmail.com>
2017-08-31 16:44:02 -0400

    adding motor section

--- src/App.vue
+++ src/App.vue
@@ -222,6 +222,25 @@ Now you have to look at all the parts holistically. For example your wheels and 
         </div>
       </div>
 
+				<div class="col-12">
+          <div class="section-title">
+            <div class="timberframing">Motor
+            <img class=""
+              src="https://png.icons8.com/engine/ios7/75"
+              height="35">
+            </div>
+          </div>
+          <p>
+            The most popular motor types you will see are belt drive single/dual motor and hub motor.
+            For this build I'll be using a single belt drive motor. Hub motors are built into the wheel and will be more efficient as there will be less drag. They tend to be smoother delivering acceleration as well.
+
+            With motors you will see a kV rating, which is how many rotations per minute (RPM) the motor will attempt to spin per volt applied.
+
+
+          </p>
+        </div>
+      </div>
+
 		</div>
   </div>
 </template>
