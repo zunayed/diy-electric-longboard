3e84242ae64dd14adbd2284ca45cb531688da439
Zunayed Ali Morsalin <zunayed@gmail.com>
2017-09-04 09:00:07 -0400

    adding red widget, and battery section

--- src/App.vue
+++ src/App.vue
@@ -57,7 +57,7 @@ Now you have to look at all the parts holistically. For example your wheels and 
 
 					<div class="col-md-2">
             <img class="icon" src="https://png.icons8.com/settings/ios7/75" height="75">
-						<span class="title">belts</span>
+						<span class="title">Belts</span>
           </div>
 
 					<div class="col-md-2">
@@ -146,6 +146,17 @@ Now you have to look at all the parts holistically. For example your wheels and 
             </div>
 				</div>
 
+        <div class="col-xs-2">
+            <div class="red widget">
+              <span class="line1">Saturday</span>
+              <span class="line2">Sept 2</span>
+							<hr/>
+              <p >
+								Recieved some parts!
+              </p>
+            </div>
+				</div>
+
       </div>
 
       <hr/>
@@ -222,24 +233,85 @@ Now you have to look at all the parts holistically. For example your wheels and 
         </div>
       </div>
 
-				<div class="col-12">
+      <hr/>
+
+      <div class="col-12">
+        <div class="section-title">
+          <div class="timberframing">Motor
+          <img class=""
+            src="https://png.icons8.com/engine/ios7/75"
+            height="35">
+          </div>
+        </div>
+        <p>
+          The most popular motor types you will see are belt drive single/dual motor and hub motor.
+          For this build I'll be using a single belt drive motor. Hub motors are built into the wheel and will be more efficient as there will be less drag. They tend to be smoother delivering acceleration as well.
+
+          With motors you will see a kV rating, which is how many rotations per minute (RPM) the motor will attempt to spin per volt applied. We'll be using this number to help calculate range and top speed
+
+
+        </p>
+      </div>
+
+      <hr/>
+
+        <div class="col-12">
           <div class="section-title">
-            <div class="timberframing">Motor
+            <div class="timberframing">Battery
             <img class=""
-              src="https://png.icons8.com/engine/ios7/75"
+              src="https://png.icons8.com/car-battery/ios7/75"
               height="35">
             </div>
           </div>
           <p>
-            The most popular motor types you will see are belt drive single/dual motor and hub motor.
-            For this build I'll be using a single belt drive motor. Hub motors are built into the wheel and will be more efficient as there will be less drag. They tend to be smoother delivering acceleration as well.
+            Entire books can be written about batteries! One of the most complex parts of the build there is quite a bit of information to digest. Please read up on battery safety as incorrect usage can be catastrophic! There are a few type of rechargeable batteries you can choose from. Lets go through some -
 
-            With motors you will see a kV rating, which is how many rotations per minute (RPM) the motor will attempt to spin per volt applied.
+            Lipo
 
+            Li-Ion
+            I'm going with LIPO for my first build to get a better understanding of the tech, how to balance cells, maintenance. Li-Ion are more work to assemble and significantly more expensive. I definitely don't want to be out hundreds of dollars due to improper usage.
 
+            I think everyone should consider using LIPO for their first build, Its a great way to learn about battery technology & balancing cells & battery handling/maintenance & wiring etc.
+            A LiPo cell has a nominal voltage of 3.7V. So when you see "2S" it means there are 2 cells in series making it 7.4Vk
           </p>
+
+          <div class="row">
+            <div class="col-4 widget-large">
+              <span class="line1"><i class="fa fa-check" aria-hidden="true"></i>LIPO</span>
+              <hr/>
+              <p>
+                <ul class="pro-con-list">
+                  <li><i class="fa fa-plus"></i> Low cost</li>
+                  <li><i class="fa fa-plus"></i> No need for battery management system</li>
+                  <li><i class="fa fa-minus"></i> Needs a special balance charger to make full use of batteries</li>
+                  <li><i class="fa fa-minus"></i> Can come in different sizes</li>
+                </ul>
+              </p>
+            </div>
+
+            <div class="col-4 widget-large">
+              <span class="line1">Li-Ion</span>
+              <hr/>
+              <p>
+                <ul class="pro-con-list">
+                  <li><i class="fa fa-plus"></i> Comes in standard sizes</li>
+                  <li><i class="fa fa-plus"></i> Large companies invest heavily in battery research and you can reap those beefits</li>
+                  <li><i class="fa fa-plus"></i> More physically robust</li>
+                  <li><i class="fa fa-plus"></i> Can come in different sizes</li>
+                  <li><i class="fa fa-plus"></i> Less prone to thermal runaway and fire</li>
+                  <li><i class="fa fa-minus"></i> Much higher cost $$</li>
+                  <li><i class="fa fa-minus"></i> If you want to diy the battery pack you will need special tooling
+                  </li>
+                  <!--+ higher voltages? allow for less heat?-->
+                </ul>
+              </p>
+            </div>
+
+
+          </div>
         </div>
-      </div>
+
+      </div><!-- End of details -->
 
 		</div>
   </div>
@@ -350,7 +422,7 @@ body {
 }
 
 .title {
-  color: #282825;
+  color: #969292;
   font-size: 16px;
   text-align: center;
   line-height: 14px;
@@ -378,6 +450,10 @@ journal area
 
 .widget.green {
 	background-color: #b1e2c9;
+}
+
+.widget.red {
+	background-color: rgba(231, 76, 60, 0.3);
 }
 
 .widget .line1 {
